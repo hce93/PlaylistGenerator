5edf986616c5182795963c14204e963ca6a8a452
Henry <henry.evison@icloud.com>
2024-10-21 14:35:10 +0100

    Initial Commit

---
+++ PlaylistGenerator/PlaylistGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistGeneratorApp.swift
+//  PlaylistGenerator
+//
+//  Created by Henry Evison on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlaylistGeneratorTests/PlaylistGeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistGeneratorTests.swift
+//  PlaylistGeneratorTests
+//
+//  Created by Henry Evison on 21/10/2024.
+//
+
+import Testing
+@testable import PlaylistGenerator
+
+struct PlaylistGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
